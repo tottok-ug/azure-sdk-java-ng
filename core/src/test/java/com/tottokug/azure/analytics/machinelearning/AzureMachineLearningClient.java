1d320a1aa1b0ca2898223ddb21f80bcc5626d41f
aucfan-tokugami <tokugami@aucfan.com>
2015-08-13 07:57:08 +0900

    machinelearning/pom

---
+++ core/src/test/java/com/tottokug/azure/analytics/machinelearning/AzureMachineLearningClient.java
@@ -0,0 +1,7 @@
+package com.tottokug.azure.analytics.machinelearning;
+
+import com.tottokug.azure.AzureClient;
+
+public interface AzureMachineLearningClient extends AzureClient {
+
+}
---
+++ core/src/test/java/com/tottokug/azure/analytics/machinelearning/AzureMachineLearningRestClient.java
@@ -0,0 +1,22 @@
+package com.tottokug.azure.analytics.machinelearning;
+
+import com.tottokug.azure.AzureRequest;
+import com.tottokug.azure.AzureResponse;
+import com.tottokug.azure.AzureRestClient;
+import com.tottokug.azure.AzureRestRequest;
+import com.tottokug.azure.AzureRestResponse;
+
+public class AzureMachineLearningRestClient implements AzureMachineLearningClient, AzureRestClient {
+
+	@Overwrite
+	public AzureResponse execute(AzureRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AzureRestResponse request(AzureRestRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
